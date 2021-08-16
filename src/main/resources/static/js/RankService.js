46a6994879d0e07e898bc036054cbf36d2bee3e7
JangJooHyuck <unodos4@infobank.net>
2021-08-16 22:36:09 +0900

    rank_html

--- src/main/resources/static/js/RankService.js
+++ src/main/resources/static/js/RankService.js
@@ -4,11 +4,12 @@ function tabSetting() {
     $($('.current').find('a').attr('href')).show();
 
     // Tab 메뉴 클릭 이벤트 생성
-    $('li').click(function (event) {
+    $('li1').click(function (event) {
         var tagName = event.target.tagName; // 현재 선택된 태그네임
         var selectedLiTag = (tagName.toString() == 'A') ? $(event.target).parent('li') : $(event.target); // A태그일 경우 상위 Li태그 선택, Li태그일 경우 그대로 태그 객체
         var currentLiTag = $('li[class~=current]'); // 현재 current 클래그를 가진 탭
         var isCurrent = false;  
+        console.log(tagName);
         
         // 현재 클릭된 탭이 current를 가졌는지 확인
         isCurrent = $(selectedLiTag).hasClass('current');
