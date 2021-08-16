83171d2fcc36a9a41b283a34fb3567e56445e3e6
JangJooHyuck <unodos4@infobank.net>
2021-08-16 23:25:20 +0900

    123

--- src/main/resources/static/js/RankService.js
+++ src/main/resources/static/js/RankService.js
@@ -1,33 +1,14 @@
-function tabSetting() {
-    // 탭 컨텐츠 hide 후 현재 탭메뉴 페이지만 show
-    $('.tabPage').hide();
-    $($('.current').find('a').attr('href')).show();
+//tab 화면 만들기
+$(document).ready(function(){
+	
+	$('ul.tabs li').click(function(){
+		var tab_id = $(this).attr('data-tab');
 
-    // Tab 메뉴 클릭 이벤트 생성
-    $('li1').click(function (event) {
-        var tagName = event.target.tagName; // 현재 선택된 태그네임
-        var selectedLiTag = (tagName.toString() == 'A') ? $(event.target).parent('li') : $(event.target); // A태그일 경우 상위 Li태그 선택, Li태그일 경우 그대로 태그 객체
-        var currentLiTag = $('li[class~=current]'); // 현재 current 클래그를 가진 탭
-        var isCurrent = false;  
-        console.log(tagName);
-        
-        // 현재 클릭된 탭이 current를 가졌는지 확인
-        isCurrent = $(selectedLiTag).hasClass('current');
-        
-        // current를 가지지 않았을 경우만 실행
-        if (!isCurrent) {
-            $($(currentLiTag).find('a').attr('href')).hide();
-            $(currentLiTag).removeClass('current');
+		$('ul.tabs li').removeClass('current');
+		$('.tab-content').removeClass('current');
 
-            $(selectedLiTag).addClass('current');
-            $($(selectedLiTag).find('a').attr('href')).show();
-        }
+		$(this).addClass('current');
+		$("#"+tab_id).addClass('current');
+	})
 
-        return false;
-    });
-}
-
-$(function () {
-    // 탭 초기화 및 설정
-    tabSetting();
-});
+})
